60575c75b83fd8b23acbc8f050d17dc29e3ea295
tientran1234 <tranngoctien29112003@gmail.com>
2025-06-08 00:54:29 +0700

    add common module

--- apps/provider/src/providers.module.ts
+++ apps/provider/src/providers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ManageServicesService } from './manage-service/manage-services.service';
 import { ManageServicesController } from './manage-service/manage-services.controller';
 import { ManageServicesModule } from './manage-service/manage-services.module';
-
+import { CommonModule } from 'libs/common/src';
+import { ConfigModule } from 'libs/common/src/modules/config.module';
 @Module({
-  imports: [ManageServicesModule],
+  imports: [ManageServicesModule, CommonModule, ConfigModule],
   controllers: [ManageServicesController],
   providers: [ManageServicesService],
 })
