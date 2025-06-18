850a8bcc65ebf92081dc954285e463b3d6b6d8fa
tientran1234 <tranngoctien29112003@gmail.com>
2025-06-18 11:12:23 +0700

    fix error controller

--- apps/provider/src/manage-service/manage-services.module.ts
+++ apps/provider/src/manage-service/manage-services.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ManageServicesService } from './manage-services.service';
 import { ManageServicesRepository } from './manager-service.repo';
+import { ManageServicesController } from './manage-services.controller';
 
 
 @Module({
+  controllers: [ManageServicesController],
   providers: [ManageServicesService, ManageServicesRepository],
   exports: [ManageServicesService, ManageServicesRepository],
 })
